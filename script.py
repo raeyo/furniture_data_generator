b325ef32c35f690be743756c8224fcebf6131ac6
raeyo <raeyo.kang@gmail.com>
2020-09-17 15:37:59 +0900

    edit script

--- script.py
+++ script.py
@@ -4,6 +4,5 @@ import sys
 import numpy as np
 
 while True:
-    p_id = np.random.randint(1, 100)
-    os.system(f"python dataGenerator.py --headless --pid {p_id}")
+    os.system("python dataGenerator.py --headless --pid {}".format(sys.argv[1]))
     # os.system("xvfb-run -a python dataGenerator.py --headless --pid {}".format(sys.argv[1]))
